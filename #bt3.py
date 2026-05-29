6bf668ff4295e1ae566d652ee6cca11d2fd72e1d
DuyXinkGai <leefusdii5@gmail.com>
2026-05-29 07:01:20 +0700

    Update #bt3.py

--- #bt3.py
+++ #bt3.py
@@ -1,6 +1,5 @@
 #bt3
 
-def main():
     ten_bn = input("Nhập họ và tên bệnh nhân: ").strip()
     ma_ba = input("Nhập mã bệnh án: ").strip()
     khoa_kham = input("Nhập khoa/phòng khám chỉ định: ").strip()
@@ -8,4 +7,3 @@ def main():
     print()
     print(f"Bệnh nhân: {ten_bn} - Mã BA: {ma_ba} - Chuyển tới: {khoa_kham}")
 
-    main()
